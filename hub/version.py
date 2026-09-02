c17849c0bd4ac060ee8dfd17456cd03f816e5fc9
Todd <todd@smart1marketing.com>
2026-09-02 18:35:19 -0400

    Release Smart Hub 1.64.21

--- hub/version.py
+++ hub/version.py
@@ -9,9 +9,9 @@ Bump VERSION whenever code is deployed. BUILD_DATE is the date of that bump.
 import os
 import subprocess
 
-VERSION = "1.64.20"
+VERSION = "1.64.21"
 BUILD_DATE = "2026-09-02"
-CODENAME = "SmartForecast"
+CODENAME = "Guided Ads Generator"
 
 _sha_cache: str | None = None
 
